5864f4fa9d77b23602636120b4b3c0eb4c219305
Mahmudul Islam Shuvo <mahmudulislammern@gmail.com>
2026-02-18 12:31:29 +0600

    Add GitHub sign-in functionality and refactor login page with dedicated sign-in buttons

--- actions/index.js
+++ actions/index.js
@@ -3,9 +3,13 @@
 import { signOut, signIn } from "@/auth";
 
 export async function handleSignOut() {
-  await signOut();
+  await signOut({ redirectTo: "/" });
 }
 
 export async function handleSigIn() {
   await signIn("google", { redirectTo: "/booking" });
+}
+
+export async function handleGitHubSignIn() {
+  await signIn("github", { redirectTo: "/booking" });
 }
